58aad7bc825964837dae55d03916c307c46474e7
Namita mehra <namitamehra@Namitas-MacBook-Air.local>
2025-11-03 20:30:01 +0530

    minor cors error fix

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const prisma = new PrismaClient()
 app.use(express.json())
 app.use(cors({
   allowedHeaders:["Content-Type","Authorization"],
-  origin: ["http://localhost:5173","https://joineazy-frontend.vercel.app/"]
+  origin: ["http://localhost:5173","https://joineazy-frontend.vercel.app"]
 }))
 
 app.post("/api/register", async (req, res) => {
